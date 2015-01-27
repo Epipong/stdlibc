ecbc7108a6570f6aef65471c73231dea63adb087
tran_y <davy.tran@epitech.eu>
2015-01-27 14:52:56 +0100

    move file

--- stdlibc/include/_string.h
+++ stdlibc/include/_string.h
@@ -1,1 +1,62 @@
-../lib/string/_string.h
+#ifndef _STRING_H_
+# define _STRING_H_
+
+# include <stdlib.h>
+# include <stdbool.h>
+
+# include "iterator.h"
+
+# define NPOS		(size_t)-1
+# define ALLOC_LENGTH	16
+# define BUFFER_LENGTH	16
+
+# ifndef STRING_CLASS
+#  define STRING_CLASS
+
+typedef struct s_string string;
+
+struct		s_string
+{
+  char		*str;
+  size_type	size;
+  size_type	length;
+};
+
+struct		s_string_class
+{
+  void		(*constructor)(string *this, char const *s);
+  void		(*destructor)(string *this);
+
+  void		*(*begin)(string *this);
+  void		*(*end)(string *this);
+
+  size_t	(*size)(string *this);
+  void		(*resize)(string *this, size_t n);
+  size_t	(*capacity)(string *this);
+  void		(*clear)(string *this);
+  bool		(*empty)(string *this);
+
+  char		(*at)(string *this, size_t pos);
+
+  char const	*(*append)(string *this, char const *str);
+  void		(*push_back)(string *this, char c);
+  char const	*(*assign)(string *this, char const *str);
+  char const	*(*insert)(string *this, size_t pos, char const *str);
+  char const	*(*erase)(string *this, size_t pos, size_t len);
+  char const	*(*replace)(string *this, size_t pos, size_t len, char const *str);
+  void		(*swap)(string *this, string *str);
+
+  char const	*(*c_str)(string *this);
+  size_t	(*copy)(string *this, char* s, size_t len, size_t pos);
+  size_t	(*find)(string *this, char const *str, size_t pos);
+  char const	*(*substr)(string *this, size_t pos, size_t len);
+  int		(*compare)(string *this, char const *str);
+};
+
+extern struct s_string_class	g_string;
+
+# endif /* !STRING_CLASS */
+
+string		*to_string(int val);
+
+#endif /* !_STRING_H_ */
--- stdlibc/include/list.h
+++ stdlibc/include/list.h
@@ -1,1 +1,55 @@
-../lib/list/list.h
+#ifndef LIST_H_
+# define LIST_H_
+
+# include <stdlib.h>
+# include <stdbool.h>
+# include "iterator.h"
+
+typedef struct s_list	list;
+typedef bool (*Predicate)(const int);
+typedef bool (*Compare)(void *, void *);
+
+struct		s_list
+{
+  iterator	content;
+  size_type	size;
+};
+
+struct s_list_class
+{
+  void		(*constructor)(list *this);
+  void		(*destructor)(list *this);
+
+  iterator	(*begin)(list *this);
+  iterator	(*end)(list *this);
+
+  bool		(*empty)(list *this);
+  size_type	(*size)(list *this);
+  size_type	(*max_size)(list *this);
+
+  void		*(*front)(list *this);
+  void		*(*back)(list *this);
+
+  void		(*assign)(list *this, iterator first, iterator last);
+  void		(*push_front)(list *this, const value_type val);
+  void		(*pop_front)(list *this);
+  void		(*push_back)(list *this, const value_type val);
+  void		(*pop_back)(list *this);
+  iterator	(*insert)(list *this, iterator position, const value_type val);
+  iterator	(*erase)(list *this, iterator position);
+  void		(*swap)(list *this, list *x);
+  void		(*resize)(list *this, size_type n);
+  void		(*clear)(list *this);
+
+  void		(*splice)(list *this, iterator position, list *x);
+  void		(*remove)(list *this, const value_type val);
+  void		(*remove_if)(list *this, Predicate pred);
+  void		(*unique)(list *this);
+  void		(*merge)(list *this, list *x);
+  void		(*sort)(list *this, Compare comp);
+  void		(*reverse)(list *this);
+};
+
+extern struct s_list_class	g_list;
+
+#endif /* !LIST_H_ */
